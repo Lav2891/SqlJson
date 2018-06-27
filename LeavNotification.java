e7ddab67b7390599dbdbc20d73a2638e0ab67c39
Lavanya Varshini <33281465+Lav2891@users.noreply.github.com>
2018-06-27 23:44:33 +0530

    Add files via upload

---
+++ LeavNotification.java
@@ -0,0 +1,28 @@
+package lav.pepbill;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Ashwin on 1/29/2018.
+ */
+
+public class LeavNotification extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TOCKEN", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+}
+
